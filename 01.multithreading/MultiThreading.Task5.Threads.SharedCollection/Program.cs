8eb0513f99f86493237e4cfecfa653ecadcae487
Pavel Zaneuski <pavel.zanevsky15@gmail.com>
2023-04-15 18:12:35 +0300

    Implement tasks 5 and 6

--- 01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ 01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,17 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        private static readonly ConcurrentQueue<int> _sharedCollection = new ConcurrentQueue<int>();
+        private static readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -17,9 +23,41 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
-            // feel free to add your code
+            var addingTask = AddingTask();
+            var printingTask = PrintingTask();
 
-            Console.ReadLine();
+            Task.WaitAll(addingTask, printingTask);
+
+            Console.ReadKey();
+        }
+
+        private static Task PrintingTask()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                while (_sharedCollection.Count < 10)
+                {
+                    _semaphore.Wait();
+                    Console.WriteLine("Printing task prints - {0}.", string.Join(", ", _sharedCollection));
+                    _semaphore.Release();
+                    Task.Delay(100).Wait();
+                }
+            });
+        }
+
+        private static Task AddingTask()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _semaphore.Wait();
+                    _sharedCollection.Enqueue(i);
+                    Console.WriteLine("Adding tasks adds - {0}.", i);
+                    _semaphore.Release();
+                    Task.Delay(100).Wait();
+                }
+            });
         }
     }
 }
--- 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
+++ 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
@@ -7,12 +7,14 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Create a Task and attach continuations to it according to the following criteria:");
             Console.WriteLine("a.    Continuation task should be executed regardless of the result of the parent task.");
@@ -22,9 +24,69 @@ namespace MultiThreading.Task6.Continuation
             Console.WriteLine("Demonstrate the work of the each case with console utility.");
             Console.WriteLine();
 
-            // feel free to add your code
+            var key = Console.ReadKey().KeyChar;
+            Console.WriteLine();
 
-            Console.ReadLine();
+            switch (key)
+            {
+                case 'a':
+                    {
+                        var task = Task.Run(() =>
+                        {
+                            Console.WriteLine("Parent task execution.");
+                            return 1;
+                        });
+                        var continuation = task.ContinueWith(t =>
+                            Console.WriteLine("Continuation task execution. Status: {0}", t.Status));
+
+                        await continuation;
+                        break;
+                    }
+                case 'b':
+                    {
+                        var task = Task.Run(() =>
+                        {
+                            Console.WriteLine("Parent task execution.");
+                            //return Task.FromException(new Exception("Custom exception."));
+                            return Task.FromCanceled(new CancellationToken(true));
+                        });
+
+                        await task.ContinueWith(t =>
+                                Console.WriteLine("Continuation task execution. Status: {0}", t.Status),
+                            TaskContinuationOptions.NotOnRanToCompletion);
+
+                        break;
+                    }
+                case 'c':
+                    {
+                        var task = Task.Run(() =>
+                        {
+                            Console.WriteLine("Parent task execution.");
+                            return Task.FromException(new Exception("Custom exception."));
+                        });
+                        await task.ContinueWith(
+                            t => Console.WriteLine("Continuation task execution. Status: {0}", t.Status),
+                            TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously);
+                        break;
+                    }
+                case 'd':
+                    {
+                        var task = Task.Run(() =>
+                        {
+                            Console.WriteLine("Parent task execution.");
+                            return Task.FromCanceled(new CancellationToken(true));
+                        });
+                        await task.ContinueWith(
+                            t => Console.WriteLine("Continuation task execution. Status: {0}", t.Status),
+                            TaskContinuationOptions.OnlyOnCanceled | TaskContinuationOptions.LongRunning);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Wrong key.");
+                        break;
+                    }
+            }
         }
     }
 }
